0962592f46afd3878a0a20b7b160838cd46745d8
Akeksandr <sash.ban2013@yandex.ru>
2024-10-17 22:21:20 +0300

    without test commit

---
+++ internal/lib/api/response/response.go
@@ -0,0 +1,50 @@
+package response
+
+import (
+	"fmt"
+	"github.com/go-playground/validator/v10"
+	"strings"
+)
+
+type Response struct {
+	Status string `json:"status"`
+	Error  string `json:"error,omitempty"`
+}
+
+const (
+	statusOK    = "OK"
+	statusError = "Error"
+)
+
+func OK() Response {
+	return Response{
+		Status: statusOK,
+	}
+}
+
+func Error(msg string) Response {
+	return Response{
+		Status: statusError,
+		Error:  msg,
+	}
+}
+
+func ValidationError(errs validator.ValidationErrors) Response {
+	var errMsgs []string
+
+	for _, err := range errs {
+		switch err.ActualTag() {
+		case "required":
+			errMsgs = append(errMsgs, fmt.Sprintf("field %s is a required field", err.Field()))
+		case "url":
+			errMsgs = append(errMsgs, fmt.Sprintf("field %s is not a valid URL", err.Field()))
+		default:
+			errMsgs = append(errMsgs, fmt.Sprintf("field %s is not valid", err.Field()))
+		}
+	}
+
+	return Response{
+		Status: statusError,
+		Error:  strings.Join(errMsgs, ", "),
+	}
+}
